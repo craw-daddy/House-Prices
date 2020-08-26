49b382876ed14d3c32f02827b5e8f1e1f800a65f
Russell Martin <russell@martinandbrown.com>
2020-08-26 16:45:07 -0400

    Initial commits for many files

---
+++ app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+See this website where I have gotten the data from:
+https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
+
+Created on Wed Aug 26 11:11:36 2020
+
+@author: martin
+"""
+
+from flask import Flask, render_template, request
+
+import pandas as pd
+import gzip
+import dill
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Greetings!  Welcome to my site!"  
+
+@app.route("/about/")
+def about():
+    return "This website is to demonstrate my cool machine learning prediction method.  Isn't this great?"
+
+@app.route("/predict/", methods=["GET"])
+def predict():
+    try:
+        MSZoning = request.args.get('mszoning')
+        MSSubClass = float(request.args.get('mssubclass'))
+        LotArea = float(request.args.get('lotarea'))
+    except ValueError:
+        return 'One or more values are non-numeric!'
+    
+    data = pd.DataFrame({'MSZoning': [MSZoning],
+                         'MSSubClass': [MSSubClass],
+                         'LotArea': [LotArea]})
+    with gzip.open('models/model.dill.gzip', 'rb') as f:
+        model = dill.load(f)
+        
+    prediction = str(model.predict(data)[0])
+    
+    return prediction
+
+#http://127.0.0.1:5000/predict/?mszoning=123.45fhjdhjfdhj&mssubclass=something
+
+if __name__ == "__main__":
+    app.run()
+    
---
+++ prep.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+See this website where I have gotten the data from:
+https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
+    
+Created on Wed Aug 26 11:10:23 2020
+
+@author: martin
+
+A file to prepare a model for deployment.  
+"""
+
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+
+import gzip
+import dill
+
+X_train = pd.read_csv('data/train.csv')
+y_train = X_train.pop('SalePrice').values
+
+model = Pipeline([
+        ('features', ColumnTransformer([
+                ('categorical', 
+                 OneHotEncoder(handle_unknown='ignore'), 
+                 ['MSZoning']),
+                ('numeric', StandardScaler(), 
+                 ['MSSubClass', 'LotArea'])])),
+        ('estimator', GridSearchCV(Ridge(), 
+                    param_grid={'alpha': np.logspace(-2, 3, 20)}, 
+                                   cv=5, verbose=1))
+])
+
+model.fit(X_train[['MSZoning','MSSubClass','LotArea']], y_train)
+print(model.score(X_train[['MSZoning','MSSubClass','LotArea']], y_train))
+
+with gzip.open('models/model.dill.gzip', 'wb') as f:
+    dill.dump(model, f, recurse=True)
+    
